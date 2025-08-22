abbccb35981b6ff7fe69b7160122f735a551cbe2
Hellorld! <jeremy.thornton@nhs.net>
2025-08-22 13:31:33 +0100

    Create contract.c

---
+++ src/CONTRACT/contract.c
@@ -0,0 +1,31 @@
+#include "contract.h" 
+
+static inline void _contract_fail(
+    const char *cond,
+    const char *msg,
+    const char *file,
+    int line
+) {
+    time_t now = time(NULL);
+    struct tm *tm_info;
+    char datetime[20]; // YYYY-MM-DD HH:MM:SS\0
+    const char *filename = file;
+
+    // Extract just the filename portion
+    const char *last_slash = strrchr(file, '\\');
+    if (!last_slash) last_slash = strrchr(file, '/');
+    if (last_slash) filename = last_slash + 1;
+
+    tm_info = localtime(&now);
+    strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M:%S", tm_info);
+
+    fprintf(stderr, "[%s] %s:%d|%s|%d(%s)|%s\n",
+            datetime,
+            filename,
+            line,
+            cond,
+            errno,
+            contract_strerror(errno),
+            msg);
+    abort();
+}
